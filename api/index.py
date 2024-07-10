f09145458d5f877a0049948230fd7d61966e2c30
Azhar Rahadian <a.rahadian@tu-braunschweig.de>
2024-07-10 18:59:46 +0200

    updated some api for testing

--- api/index.py
+++ api/index.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/api/hello")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello World! This API is now working yippie!"}
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the site!"}
