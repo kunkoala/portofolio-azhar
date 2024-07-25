11185b9909142bedbc8e977b1c59beea96964cff
Azhar Rahadian <a.rahadian@tu-braunschweig.de>
2024-07-25 13:49:52 +0200

    added a working config for the setup of the project

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,12 +1,49 @@
-import os
-from pydantic_settings import BaseSettings
+import secrets
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import MariaDBDsn, computed_field
+from pydantic_core import MultiHostUrl
+from typing import Annotated, Any, Literal
+
+'''
+filename: config.py
+This file serves as the global config file for the API Project. 
+It stores application constants and loads .env
+'''
 
 
 class Settings(BaseSettings):
-    ENV: str = os.getenv('ENV', default='development')
-    DATABASE_URL: str = os.getenv('DATABASE_URL')
-    
-    class Config:
-        env_file = f".env.{os.getenv('ENV','development')}.local"
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, validate_default=False
+    )
+
+    # following the full-stack fast-api template
+    APP_NAME: str
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    DOMAIN: str = "localhost"
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+
+    MARIADB_SERVER: str
+    MARIADB_PORT: int = 3306  # default is 3306
+    MARIADB_USER: str
+    MARIADB_PASSWORD: str
+    MARIADB_DB: str
+
+    @computed_field  # for docs: https://docs.pydantic.dev/2.0/usage/computed_fields/
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> MariaDBDsn:
+        return MultiHostUrl.build(
+            scheme="mariadb+mariadbconnector",
+            username=self.MARIADB_USER,
+            password=self.MARIADB_PASSWORD,
+            host=self.MARIADB_SERVER,
+            port=self.MARIADB_PORT,
+            path=self.MARIADB_DB,
+        )
+
 
 settings = Settings()
+
+print(settings.model_dump())
