9a39f04e0c0738aa29a316ea6c21c7a908eddb9a
Azhar Rahadian <a.rahadian@tu-braunschweig.de>
2024-07-25 13:51:31 +0200

    load the proper SQLALCHEMY URI

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,15 +1,12 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 from app.config import settings
 
-# Load environment variables from .env file
-load_dotenv(f".env.{settings.ENV}.local")
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+SQLALCHEMY_DATABASE_URL = settings.SQLALCHEMY_DATABASE_URI
 print(f"SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(str(SQLALCHEMY_DATABASE_URL))
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False, bind=engine)
