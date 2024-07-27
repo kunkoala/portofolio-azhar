d6f20b684d3b33dbe82f618b6c8923d0ebb78c97
kunkoala <azharffrahadian@gmail.com>
2024-07-28 00:38:49 +0200

    removed email from the model, so that users just input their name and message

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Text, func
+from pydantic import Field, BaseModel, EmailStr
 
 Base = declarative_base()
 
@@ -8,9 +9,9 @@ class Guestbook(Base):
     __tablename__ = "guestbook"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False, index=True)
+    created_by = Column(String(80), nullable=False)
     email = Column(String(255), nullable=False)
     message = Column(Text, nullable=False)
-    created_by = Column(String(255), nullable=False)
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+
