cf3f32518771d92464dd69733d076b4ecb95e7e2
kunkoala <azharffrahadian@gmail.com>
2024-07-30 15:08:06 +0200

    solved the problem of pydantic field validation by using the default of none and data optional, to let the database handle the data

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,30 +1,28 @@
 from sqlmodel import Field, SQLModel, Column
 from sqlalchemy import DateTime, func, text
-from typing import Optional
 import datetime
 
-
+"""
+The GuestbookBase model is a shared model that contains the common properties of the GuestbookCreate and GuestbookPublic models.
+"""
 # shared properties
 class GuestbookBase(SQLModel):
-    created_by: str = Field(max_length=80)
-    message: str = Field(max_length=80)
+    created_by: str = Field(min_length=1, max_length=80)
+    message: str = Field(min_length=1, max_length=255)
 
 # properties to receive on item creation    
 class GuestbookCreate(GuestbookBase):
-    created_by: str = Field(min_length=1, max_length=80)
-    message: str = Field(min_length=1, max_length=80)
+    # TODO: later on, use the user.id and user.username as the created_by
+    pass
     
 
-# properties to return via API, id is required
+# properties to return via API, id, created_at and updated_at is required
 class GuestbookPublic(GuestbookBase):
     id: int
     
     # remember to use datetime.datetime instead of datetime for the type hint
     created_at: datetime.datetime
     updated_at: datetime.datetime
-    
-    class Config:
-        orm_mode = True
 
 
 class Guests(SQLModel):
@@ -36,27 +34,18 @@ class Guests(SQLModel):
 class Guestbook(GuestbookBase, table=True):
     __tablename__ = "guestbook"
 
+    """
+    the id, created_at, and updated_at are not required in the GuestbookBase model, since they are handled by the database. 
+    
+    The default values in the python model will be set to None, and the database will handle the default values for the created_at and updated_at fields.
+    """
     id: int | None = Field(default=None, primary_key=True)
-    created_by: str = Field(max_length=80)
-    message: str = Field(max_length=80)
-    created_at: datetime.datetime = Field(sa_column=Column(DateTime, server_default=func.now()))
-    updated_at: Optional[datetime.datetime] = Field(sa_column=Column(DateTime, server_default=func.now(), onupdate=func.now()))
+    created_at: datetime.datetime | None = Field(default=None, sa_column=Column(DateTime, server_default=func.now(), nullable=False))
+    updated_at: datetime.datetime | None = Field(default=None, sa_column=Column(DateTime, server_default=func.now(), onupdate=func.now()))
     
     
 class Message(SQLModel):
     message: str
-
-
-'''
-Use Pydantic's orm_mode¶
-Now, in the Pydantic models for reading, Item and User, add an internal Config class.
-
-This Config class is used to provide configurations to Pydantic.
-
-In the Config class, set the attribute orm_mode = True
-
-Pydantic's orm_mode will tell the Pydantic model to read the data even if it is not a dict, but an ORM model (or any other arbitrary object with attributes).
-'''
 
 
 
