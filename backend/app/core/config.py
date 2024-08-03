d1e84d61d77e4ba916967e7843971262b4ff5990
kunkoala <azharffrahadian@gmail.com>
2024-08-03 21:09:35 +0200

    updated config for loading env and the .env.example

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,12 @@
 import secrets
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import MariaDBDsn, computed_field
+from pydantic import (
+    MariaDBDsn,
+    computed_field,
+    AnyUrl,
+    HttpUrl,
+    BeforeValidator,
+)
 from pydantic_core import MultiHostUrl
 from typing import Annotated, Any, Literal
 
@@ -9,6 +15,14 @@ filename: config.py
 This file serves as the global config file for the API Project. 
 It stores application constants and loads .env
 '''
+
+
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",")]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
 
 
 class Settings(BaseSettings):
@@ -25,11 +39,16 @@ class Settings(BaseSettings):
     DOMAIN: str = "localhost"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
+    BACKEND_CORS_ORIGINS: Annotated[list[AnyUrl] | str, BeforeValidator(parse_cors)] = []
+
     MARIADB_SERVER: str
     MARIADB_PORT: int = 3306  # default is 3306
     MARIADB_USER: str
     MARIADB_PASSWORD: str
     MARIADB_DB: str
+
+    FIRST_SUPERUSER: str
+    FIRST_SUPERUSER_PASSWORD: str
 
     @computed_field  # for docs: https://docs.pydantic.dev/2.0/usage/computed_fields/
     @property
