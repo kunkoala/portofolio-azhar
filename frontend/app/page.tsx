a6ae67f1745db1894ba9ef0002cbb3397bf7d9d7
kunkoala <azharffrahadian@gmail.com>
2024-08-03 22:18:41 +0200

    changed query url

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { getByQuery } from "@/lib/api";
 
 export default async function Home() {
-  const data = await getByQuery("/api/hello");
+  const data = await getByQuery("/project_settings/hello");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
