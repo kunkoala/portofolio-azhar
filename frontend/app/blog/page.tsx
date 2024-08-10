c9b2efe28fc4178ece1f07f3e014d79170a4819e
kunkoala <azharffrahadian@gmail.com>
2024-08-10 18:08:53 +0200

    added a blog and guestbook page

---
+++ frontend/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <h1>This is a blog page</h1>
+    </main>
+  );
+}
---
+++ frontend/app/guestbook/page.tsx
@@ -0,0 +1,7 @@
+export default function GuestbookPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <h1>This is a guestbook page</h1>
+    </main>
+  );
+}
