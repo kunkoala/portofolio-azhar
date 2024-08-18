93d6e5ec93ab50340357f41809829b6727faf36b
kunkoala <azharffrahadian@gmail.com>
2024-08-19 00:59:33 +0200

    chore: Update footer and hero section styling, also dropdown menu shadcn-ui for the dark and lightmode toggle

--- frontend/components/Navbar/footer.tsx
+++ frontend/components/Navbar/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="bg-foreground text-sm text-background py-4 min-h-32">
+    <footer className="dark:bg-gray-950 text-sm py-4 min-h-32">
       <div className="container mx-auto flex items-center justify-between">
         <p>&copy; {new Date().getFullYear()} Azhar Rahadian</p>
       </div>
