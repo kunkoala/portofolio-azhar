e877f6bc2dc583009ee3e23816c65fa7902f8f75
kunkoala <azharffrahadian@gmail.com>
2024-08-19 00:59:10 +0200

    chore: Update footer and hero section styling

--- frontend/app/(landing)/hero-section.tsx
+++ frontend/app/(landing)/hero-section.tsx
@@ -1,10 +1,19 @@
 export function HeroSection() {
   return (
     <section
-      className="min-h-screen flex flex-col items-center justify-center bg-gray-100"
+      className="h-[50vh] flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-800"
       id="hero-section"
     >
-      <h1>Hero Section</h1>
+      <main className="w-full">
+        <div className="flex flex-col items-center justify-center px-4 md:px-6">
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            Azhar Rahadian
+          </h1>
+          <p className="text-lg md:text-xl mt-2 max-w-[600px] text-center">
+            Full-Stack Developer | Composer{" "}
+          </p>
+        </div>
+      </main>
     </section>
   );
 }
